5617d64c3b7c4c6383d8f9c5ad27e070a33ab2f4
salaheddine akil <s.akil@sahamassurance.siege.maroc>
2025-05-26 10:25:40 +0100

    return id with response

--- Models/Destinataire.cs
+++ Models/Destinataire.cs
@@ -6,8 +6,8 @@ namespace Gestion_Destinataire.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Nom { get; set; }
-        public string Prenom { get; set; }
-        public string Email { get; set; }
+        public required string Nom { get; set; }
+        public required string Prenom { get; set; }
+        public required string Email { get; set; }
     }
 }
